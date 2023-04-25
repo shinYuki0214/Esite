275c4353f5a9bd6fa7725b54fc387891800f9f8b
shinYuki0214 <sin2022web@gmail.com>
2023-04-25 10:43:56 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,6 @@
     コンポーネントテスト１
     <x-tests.card title="タイトル" content="こんてんと" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル"></x-tests.card>
+    <x-tests.card title="cssを変更したい" class="bg-red-300"></x-tests.card>
+
 </x-tests.app>
