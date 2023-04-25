6c32f8b70da995b7514428edd0a515b20472f8c1
shinYuki0214 <sin2022web@gmail.com>
2023-04-25 10:58:32 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,10 @@
+<div>
+    クラスベースのコンポーネント
+    <div>
+        {{ $classBaseMessage }}
+    </div>
+    <div>
+        {{ $defaultMessage }}
+    </div>
+    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,6 +2,6 @@
     <x-slot name="header">
         ヘッダー2
     </x-slot>
-    コンポーネントテスト2
-
+    <x-test-class-base classBaseMessage="メッセージです"></x-test-class-base>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期変更"></x-test-class-base>
 </x-tests.app>
